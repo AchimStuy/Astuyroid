e6509a8cd2b32fb12fb75af7fdec6e4f28383063
AchimRies <AchimRies@users.noreply.github.com>
2026-03-30 23:40:28 +0200

    Signed build without GMS

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+podman pull lineageos4microg/docker-lineage-cicd
+
+podman run \
+    -e "BRANCH_NAME=lineage-22.2" \
+    -e "BUILD_TYPE=user" \
+    -e "DEVICE_LIST=sargo,walleye" \
+    -e "MAKE_IMG_ZIP_FILE=true" \
+    -e "WITH_AVB=true" \
+    -e "WITH_GMS=false" \
+    -v "./cache:/srv/ccache" \
+    -v "./keys:/srv/keys" \
+    -v "./local_manifests:/srv/local_manifests" \
+    -v "./logs:/srv/logs" \
+    -v "./src:/srv/src" \
+    -v "./userscripts:/srv/userscripts" \
+    -v "./zips:/srv/zips" \
+    lineageos4microg/docker-lineage-cicd
+    #-e "CLEAN_AFTER_BUILD=false" \
